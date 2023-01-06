21b870e08a8670f4226ce0f28aba5ad3582444a6
Ahzam Khan <ahzam.main@gmail.com>
2023-01-06 16:09:06 +0530

    nucleotide counter

---
+++ dna.py
@@ -0,0 +1,21 @@
+def nucleotide(dna):
+    A = dna.count("A")
+    C= dna.count("C")
+    G = dna.count("G")
+    T = dna.count("T")
+
+    return("{} {} {} {}".format(A,C,G,T))
+
+
+dna = open("rosalind_dna.txt")
+res = str(nucleotide(dna.read()))
+output = open("output_dna.txt","w+")
+output.write(res)
+output.close()
+
+
+
+
+
+
+    
